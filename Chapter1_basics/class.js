7f58e64e6f867e620179e3b0e7d99fb6684d4946
joonho_Lee <goodfluence@naver.com>
2021-04-09 06:45:07 +0900

    OOP Chapter 1) Class

---
+++ Chapter1_basics/class.js
@@ -0,0 +1,84 @@
+"use strict";
+
+// class : template
+// object : instance of a class
+// Javascruipt classes
+// - introdued in ES6(2015)
+// - syntactical sugar over prototype-based inheritance (언어 구현상 원리)
+
+// 1. Class declarations
+class Person {
+  // constructor
+  constructor(name, age) {
+    // fields
+    this.name = name;
+    this.age = age;
+  }
+
+  // methods
+  speak() {
+    console.log(`${this.name}: hello!`);
+  }
+}
+
+const joonho = new Person("joonho", 20);
+console.log(joonho.name);
+console.log(joonho.age);
+joonho.speak();
+
+// 2. Getter and Setters
+class User {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(value) {
+    this._age = value < 0 ? 0 : value;
+  }
+}
+
+const user1 = new User("Steve", "Jobs", -1);
+console.log(user1.age);
+
+// 3. Inheritance
+// a way for one class to extend another class.
+
+class Shape {
+  constructor(width, height, color) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  draw() {
+    console.log(`drawing ${this.color} color of`);
+  }
+
+  getArea() {
+    return this.width * this.height;
+  }
+}
+
+class Rectangle extends Shape {}
+class Triangle extends Shape {
+  draw() {
+    super.draw();
+    console.log("🔺");
+  }
+  getArea() {
+    return (this.width * this.height) / 2;
+  }
+}
+
+const rectangle = new Rectangle(20, 20, "blue");
+rectangle.draw();
+console.log(rectangle.getArea());
+const triangle = new Triangle(20, 20, "red");
+triangle.draw();
+console.log(triangle.getArea());
