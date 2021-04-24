8269b81d3cd1c2c8957e3dffdad0bc2bf2635cec
joonho_Lee <goodfluence@naver.com>
2021-04-24 20:32:15 +0900

    OOP Chapter 7) DataStructure : Priority Queue (4) 테스트

--- Chapter2_datastructure/priorityQueue/Heap.js
+++ Chapter2_datastructure/priorityQueue/Heap.js
@@ -1,8 +1,9 @@
-export class Heap extends arrayList {
-  constructor(data = null) {
-    super(); // this.array = [], this.length = 0;
-    this.index = 1;
-    super.array[index] = data;
+import { arraylist } from "../arraylist/array.js";
+
+export class Heap extends arraylist {
+  constructor() {
+    console.log(super());
+    console.log((this.array[0] = null));
   }
 
   isEmpty() {
@@ -10,7 +11,7 @@ export class Heap extends arrayList {
   }
 
   getParentIndex(idx) {
-    return idx % 2 === 0 ? idx / 2 : idx / 2 - 1;
+    return idx % 2 === 0 ? idx / 2 : (idx - 1) / 2;
   }
 
   getLChildIndex(idx) {
@@ -23,14 +24,14 @@ export class Heap extends arrayList {
 
   getHiPriChildIndex(idx) {
     // 자식노드가 없는 경우 : 해당 인덱스의 왼쪽 자식의 값이 총 길이보다 클 때
-    if (this.getLChildIndex(idx) > this.length) {
+    if (this.getLChildIndex(idx) > this.array.length) {
       return 0;
       // 자식노드가 1개 있는 경우 : 왼쪽 자식 노드의 인덱스를 반환함
-    } else if (this.getLChildIndex(idx) === this.length) {
+    } else if (this.getLChildIndex(idx) === this.array.length) {
       return this.getLChildIndex(idx);
       // 자식노드가 2개 있는 경우, 우선순위가 더 높은 값의 인덱스를 반환해준다.
     } else {
-      if (super.array[this.getLChildIndex] < super.array[this.getRChildIndex]) {
+      if (this.array[this.getLChildIndex] < this.array[this.getRChildIndex]) {
         return this.getRChildIndex(idx);
       } else {
         return this.getLChildIndex(idx);
@@ -39,41 +40,51 @@ export class Heap extends arrayList {
   }
 
   swap(indexA, indexB) {
-    let temp = super.array[indexA];
-    super.array[indexA] = super.array[indexB];
-    super.array[indexB] = temp;
+    let temp = this.array[indexA];
+    this.array[indexA] = this.array[indexB];
+    this.array[indexB] = temp;
+    console.log("this.array", this.array);
+    console.log("this.array[indexB]", this.array[indexB]);
+    console.log("this.array[indexA]", this.array[indexA]);
   }
 
   Insert(data) {
     // ① 가장 마지막 노드의 위치에 값을 추가한다.
-    const newData = super.array.push(data);
-    let idx = super.length();
+    if (this.array.length === 1) {
+      super.push(data);
+      return this.array[this.array.length - 1];
+    }
+    super.push(data);
+    let idx = this.array.length - 1;
     let parentIndex = this.getParentIndex(idx);
 
-    // ② 부모노드와 추가된 노드의 값(우선순위)을 비교해, 우선순위가 더 높을 때까지 계속 값을 변경한다.
-    while (super.array[parentIndex] < super.array[idx]) {
-      this.swap(this.getParentIndex(idx), super.array[idx]);
-      parenIndex = this.getParentIndex(parentIndex);
+    // ② 부모노드와 추가된 노드의 값(우선순위)을 비교해, 우선순위가 더 높을 때까지 계속 값을 변경한다. 값이 작으면 우선순위가 높은 것임을 유의하자. 따라서 그에 반대되는 조건일 경우, 값을 비교하여 swap 해주면 된다.
+
+    while (this.array[parentIndex] > this.array[idx]) {
+      this.swap(this.getParentIndex(idx), idx);
+      const newParenIndex = this.getParentIndex(parentIndex);
+      parentIndex = newParenIndex;
       idx = this.getParentIndex(idx);
     }
 
-    return newData;
+    return this.array[this.array.length - 1];
   }
 
   Delete() {
-    const returnData = super.array[rootIdx];
-    const deleteData = returnData;
     let rootIdx = 1;
+    const returnData = this.array[rootIdx];
+    const deleteData = returnData;
     // ① 루트 노드를 삭제한다.
-    super.array[rootIdx] = null;
+    this.array[rootIdx] = null;
 
     // ② 가장 마지막 노드를 루트 노드에 둔다.
-    super.array[rootIdx] = super.array[super.length()];
+    this.array[rootIdx] = super.pop();
 
     let childIdx = this.getHiPriChildIndex(rootIdx);
+    console.log("childIdx", childIdx);
     // ③ 자식 노드의 우선순위가 더 낮아질 때까지 값을 변경한다.
-    while (super.array[childIdx] > super.array[rootIdx]) {
-      this.swap(super.array[childIdx], super.array[rootIdx]);
+    while (this.array[childIdx] < this.array[rootIdx]) {
+      this.swap(childIdx, rootIdx);
       childIdx = this.getHiPriChildIndex(childIdx);
       rootIdx = this.getHiPriChildIndex(rootIdx);
     }
@@ -82,6 +93,16 @@ export class Heap extends arrayList {
   }
 }
 
-const init = () => {};
+const init = () => {
+  const testHeap = new Heap();
+  testHeap.Insert(4);
+  testHeap.Insert(2);
+  testHeap.Insert(8);
+  testHeap.Insert(6);
+  testHeap.Insert(10);
+  testHeap.Insert(3);
+  testHeap.Insert(1);
+  console.log(testHeap);
+};
 
 init();
